c48e0e5b0505d17b3050a937e1b3c5fcaec26269
Gla2023 <sextocomercialmatematica@gmail.com>
2023-08-27 13:18:45 -0300

    Cambiando nombres

--- tp6/tp6ej1test.py
+++ tp6/tp6ej1test.py
@@ -4,10 +4,10 @@ Created on Sat Aug 26 19:29:22 2023
 
 @author: glady
 """
-import tp6.ej1
+import tp6ej1
 def test_freccaracteres():
     print("Testeando programa")
-    assert(frecuencia_caracter(Gladys)=={'G': 1, 'l': 1, 'a': 1, 'd': 1, 'y': 1, 's': 1})
+    assert(tp6ej1.frecuencia_caracter("Gladys")=={'G': 1, 'l': 1, 'a': 1, 'd': 1, 'y': 1, 's': 1})
     print("Funciona")
     
 test_freccaracteres()
