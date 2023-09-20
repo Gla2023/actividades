ea21df6a4b08cf9ea65710c71fd4a35aa5a9a52d
Gla2023 <143039852+Gla2023@users.noreply.github.com>
2023-09-20 18:05:20 -0300

    Update Excesodedemanda.py

--- Trabajo final de Elementos de la computación/Excesodedemanda.py
+++ Trabajo final de Elementos de la computación/Excesodedemanda.py
@@ -11,3 +11,19 @@ def exceso_demanda():
     output
     type 
     """
+import numpy as np
+from scipy.integrate import quad
+
+# Punto de equilibrio (supongamos que es x = 3)
+x_equilibrio = 3
+y_equilibrio = f(x_equilibrio) - g(x_equilibrio)
+
+# Definir la función de diferencia h(x) = f(x) - g(x)
+def h(x):
+    return f(x) - g(x)
+
+# Calcular la integral de h(x) en un intervalo [a, x_equilibrio]
+a = 0  # Puedes cambiar el límite inferior según tus necesidades
+integral_result, error = quad(h, a, x_equilibrio)
+
+print("El valor de la integral de h(x) en el i")
